3b8e91175f6c743776408f070d9d3eb61d1cffd6
ArsalaKhan1 <arsalakhan08@gmail.com>
2026-09-15 11:51:01 +0500

    Updated

--- frontend/pawpal-react/src/pages/NewPet.jsx
+++ frontend/pawpal-react/src/pages/NewPet.jsx
@@ -29,7 +29,7 @@ function NewPet(){
         const token = localStorage.getItem("token");
 
         try {
-            await api.post("/pets", formData, {
+            await api.post("/NewPet", formData, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
